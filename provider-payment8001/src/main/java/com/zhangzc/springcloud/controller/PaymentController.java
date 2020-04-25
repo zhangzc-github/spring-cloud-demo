2dd582e83785be67207b1d608bb7d45d98c9c32d
zhangzc-github <1843868984@qq.com>
2020-04-25 08:54:22 +0800

    consul完成

--- provider-payment8001/src/main/java/com/zhangzc/springcloud/controller/PaymentController.java
+++ provider-payment8001/src/main/java/com/zhangzc/springcloud/controller/PaymentController.java
@@ -4,29 +4,32 @@ import com.zhangzc.springcloud.entities.CommonResult;
 import com.zhangzc.springcloud.entities.Payment;
 import com.zhangzc.springcloud.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
+    @Value("${server.port}")
+    private String serverPort;
 
     @PostMapping("/payment/add")
     public CommonResult<Integer> add(@RequestBody Payment payment){
         Integer result = paymentService.save(payment);
         if(result>=0){
-            return new CommonResult<>(200,"成功",result);
+            return new CommonResult<>(200,"成功"+serverPort,result);
         }else{
-            return new CommonResult<>(400,"失败",result);
+            return new CommonResult<>(400,"失败"+serverPort,result);
         }
     }
     @GetMapping("/payment/query/{id}")
     public CommonResult<Payment> query(@PathVariable("id") int id){
         Payment result = paymentService.findById(id);
         if (result != null){
-            return new CommonResult<>(200,"成功",result);
+            return new CommonResult<>(200,"成功"+serverPort,result);
         }else{
-            return new CommonResult<>(400,"失败",result);
+            return new CommonResult<>(400,"失败"+serverPort,result);
         }
     }
 }
